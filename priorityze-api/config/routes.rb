b7c3a73ff47c3ee62909e818c59641f193f60dd0
Jason McGloin <jason.mcgloin@gmail.com>
2020-03-24 01:33:47 -0400

    Updates dev port, routes

--- priorityze-api/config/routes.rb
+++ priorityze-api/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+	namespace :api do
+		namespace :v1 do
+			resources :goal, only: [:index, :create, :destroy, :update]
+			resources :step, only: [:index, :create, :update, :destroy]
+		end
+	end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
