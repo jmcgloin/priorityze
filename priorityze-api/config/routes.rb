5d5fc7085fe30b65a33bc5ede48d6b7cabac0811
Jason McGloin <jason.mcgloin@gmail.com>
2020-03-29 05:39:57 -0400

    Adds gems, installs devise, creates devise model, configures devise model, creates revocation strategy

--- priorityze-api/config/routes.rb
+++ priorityze-api/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
 	namespace :api do
 		namespace :v1 do
 			resources :goal, only: [:index, :create, :destroy, :update]
 			resources :step, only: [:index, :create, :update, :destroy]
 		end
 	end
+	root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- priorityze-api/db/schema.rb
+++ priorityze-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_044532) do
+ActiveRecord::Schema.define(version: 2020_03_29_091944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 2020_03_24_044532) do
     t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
+  create_table "jwt_blacklist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["jti"], name: "index_jwt_blacklist_on_jti"
+  end
+
   create_table "steps", force: :cascade do |t|
     t.string "metric"
     t.boolean "completed", default: false
@@ -38,12 +46,16 @@ ActiveRecord::Schema.define(version: 2020_03_24_044532) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
-    t.string "password_confirmation"
     t.string "email"
     t.string "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "goals", "users"
